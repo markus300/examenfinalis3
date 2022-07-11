fd38eec41a183c93fbb9b622416c7a3f38957d94
jfaquinopando <pandolcik@gmail.com>
2022-07-10 23:55:19 -0400

    Busqueda de tweets

--- app/controladores/tweets.js
+++ app/controladores/tweets.js
@@ -12,6 +12,7 @@ exports.getData = (req, res) => {
   */
   const nombre  = req.query
   console.log('query nombre', nombre);
+  if (nombre.text == undefined) {
   model.find({
       // aqui puede ir un query
     }).sort('-idt').exec((err, docs) => {
@@ -19,7 +20,23 @@ exports.getData = (req, res) => {
         docs:docs // o simplmente docs
       })
     })
-
+  }else{
+      console.log('con parametros');
+       model.find({
+       }, (err, docs) => {
+         console.log('nombre.text;');
+         console.log(nombre.text);
+         console.log('docs?');
+         console.log(docs);
+         const aBuscar = nombre.text;
+         const newDocs = docs.filter(el => {
+           return el.comentario.includes(aBuscar)
+         });
+         res.send({
+           docs:newDocs // o simplmente docs
+         })
+       })
+  }
 }
 
 exports.getDataById = (req, res) =>{
